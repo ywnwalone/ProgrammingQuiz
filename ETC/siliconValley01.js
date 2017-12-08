e051c77c60d4960915816dedfb0db25b9abf1c5e
1048576 <ywnwalone@naver.com>
2017-12-08 18:18:38 +0900

    update ETC

---
+++ ETC/siliconValley01.js
@@ -0,0 +1,21 @@
+/*
+**
+**  정수로 된 배열이 주어지면, 각 원소가 자신을 뺀 나머지 원소들의 곱셈이 되게 하라.
+**  나누기 금지 , O(n)
+
+
+**  예)
+
+**  [1, 2, 3, 4, 5]
+**  [120, 60, 40, 30, 24]
+*/
+
+function solution(arr){
+    var answer  = '';
+
+    return answer;
+}
+
+var test1 = [1, 2, 3, 4, 5];
+
+console.log(solution(test1));
